026d2ecb94379e3c74a29db871adf27e12cea7e5
LauraGenari <lauragenari018@usp.br>
2020-07-24 14:59:21 -0300

    #deontem sextou

--- src/components/cliente/AdoptionItem.js
+++ src/components/cliente/AdoptionItem.js
@@ -1,29 +1,35 @@
-import React from 'react'
-import '../style.css'
+import React from "react";
+import "../style.css";
 
 class AdoptionItem extends React.Component {
   render() {
     return (
-      <div class='displayCard'>
-        <img alt={this.props.name} src={this.props.imgSrc} />
-        <p class='displayName'>{this.props.name}</p>
-        <div class='raceAndAge'>
-          <p class='raceLabel'>Espécie: </p>
-          <p class='raceContent'>{this.props.specie}</p>
-          <p class='raceLabel'>Raça:</p>
-          <p class='raceContent'>{this.props.race}</p>
-          <p class='ageLabel'>Idade:</p>
-          <p class='ageContent'>{this.props.age + ' anos'}</p>
-          {this.props.type === 'Venda' ? (
+      <div class="displayCard">
+        <img
+          alt={this.props.name}
+          src={this.props.imgSrc}
+          style={{ width: "100%", height: "15em" }}
+        />
+        <p class="displayName">{this.props.name}</p>
+        <div class="raceAndAge">
+          <p class="raceLabel">Espécie: </p>
+          <p class="raceContent">{this.props.specie}</p>
+          <p class="raceLabel">Raça:</p>
+          <p class="raceContent">{this.props.race}</p>
+          <p class="ageLabel">Idade:</p>
+          <p class="ageContent">{this.props.age + " anos"}</p>
+          {this.props.type === "Venda" ? (
             <>
-              <p class='raceLabel'>Preço:</p>
-              <p class='raceContent'>R$ {Number(this.props.price).toFixed(2)}</p>
+              <p class="raceLabel">Preço:</p>
+              <p class="raceContent">
+                R$ {Number(this.props.price).toFixed(2)}
+              </p>
             </>
           ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default AdoptionItem
+export default AdoptionItem;
--- src/components/cliente/ProductItem.js
+++ src/components/cliente/ProductItem.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import '../style.css'
+import React from "react";
+import "../style.css";
 
 class ProductItem extends React.Component {
   render() {
     return (
-      <div class='productItemHolder'>
-        <div class='productItem'>
+      <div class="productItemHolder">
+        <div class="productItem">
           <img alt={this.props.name} src={this.props.imgSrc} />
           <div
             style={{
-              display: 'flex',
-              marginTop: 1 + 'em',
-              fontSize: 0.85 + 'em',
+              display: "flex",
+              marginTop: 1 + "em",
+              fontSize: 0.85 + "em",
             }}
           >
-            <div style={{ width: '50%', textAlign: 'left' }}>
+            <div style={{ width: "50%", textAlign: "left" }}>
               <p>{this.props.name}</p>
             </div>
-            <div style={{ width: '50%', textAlign: 'rigth', color: 'red' }}>
-              <p>{'R$ ' + this.props.price}</p>
+            <div style={{ width: "50%", textAlign: "rigth", color: "red" }}>
+              <p>{"R$ " + this.props.price}</p>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default ProductItem
+export default ProductItem;
